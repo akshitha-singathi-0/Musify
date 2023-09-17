8fa0e13a7653dc5987f52f7678f67d84f4676ac6
akshitha-singathi-0 <singathia@yahoo.com>
2023-09-17 15:45:48 -0400

    added sample logic for generation

--- app/sketch.js
+++ app/sketch.js
@@ -1,7 +1,32 @@
-function setup() {
-  createCanvas(400, 400);
+let img;
+let c;
+
+function preload() {
+  createCanvas(1000, 1000);
+  img = loadImage('../src/sample.jpg');
 }
 
 function draw() {
-  background(220);
+  let x = random(width);
+  let y = random(height);
+  c = img.get(x, y);
+  fill(c);
+  noStroke();
+  ellipse(x, y, 4, 4);
 }
+
+/*
+function setup() {
+  size(900, 900)
+  img = loadImage('../src/sample.jpg');
+  background(0)
+}
+
+function draw() {
+
+  let x = random(width);
+  let y = random(height);
+  c = img.get(int(x), int(y));
+  fill(c);
+  ellipse(c);
+}*/
